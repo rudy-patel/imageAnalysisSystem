1e7d24eaed5c214b37a74664f9eee785deb62d20
jpaetsch <j.paetsch@live.com>
2022-03-20 17:39:11 -0600

    Set up the initial client script for nodes

--- client/client.py
+++ client/client.py
@@ -7,23 +7,31 @@ import time
 # Argument parser
 ap = argparse.ArgumentParser()
 ap.add_argument("-s", "--server-ip", required=True, help="IP address of the server this client connects to")
+ap.add_argument("-s", "--port-number", required=True, help="Port of the server this client connects to (NOTE 5555, 5556)")
 args = vars(ap.parse_args())
-# Initialize ImageSender object with the socket address of the server
-sender = imagezmq.ImageSender(connect_to="tcp://{}:5555".format(args["server_ip"]))
+# Initialize ImageSender object with the socket address of the server thread
+sender = imagezmq.ImageSender(connect_to="tcp://{}:{}".format(args["server_ip"], args["port-number"]))
 
-# Get the host name, initialize the video stream, and allow the camera sensor time to warm up
-rpiName = socket.gethostname()
+# Get the host name, initialize the video stream
+camera_id = socket.gethostname()
 vs = VideoStream(usePiCamera=True).start()
 
-#vs = VideoStream(src=0).start()  // any USB camera can be used if this line replaces the one above
+# Any USB camera can be used if this line is used instead
+# vs = VideoStream(src=0).start()
 
-# If there is lag, too many pixels are being sent (max is used with no specification)
-# Could set the video resolution here, see https://picamera.readthedocs.io/en/release-1.12/fov.html V2 camera section
+# Change to your IP stream address (need to get from manufacturer or set up)
+# See https://help.angelcam.com/en/articles/372649-finding-rtsp-addresses-for-ip-cameras-nvrs-dvrs
+# path = "rtsp://<client-ip>:<client-port>//<idk.sdp>"
+# vs = VideoStream(path)
+
+# If there is lag, too many pixels are being sent (max is used with no specification) - set video resolution
+# See https://picamera.readthedocs.io/en/release-1.12/fov.html
 # vs = VideoStream(usePiCamera=True, resolution=(xx, yy)).start()
 
- time.sleep(2.0)
+# Allow the camera sensor time to warm up
+time.sleep(2.0)
 
 while True:
 	# Read the frame from the camera, send it to the server
 	frame = vs.read()
-	sender.send_image(rpiName, frame)
+	sender.send_image(camera_id, frame)
