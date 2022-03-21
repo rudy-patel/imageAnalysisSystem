df72fa1f5efc9b45b3df86020fdfbc0b9f8a550c
jpaetsch <j.paetsch@live.com>
2022-03-21 16:18:31 -0600

    Removed unnecessary files for database classes I created

--- server/db_classes/camera.py
+++
@@ -1,9 +0,0 @@
-# from flask_sqlalchemy import SQLAlchemy
-
-
-# class Camera(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-#     name = db.Column(db.String(50), nullable=False)
-#     status = db.Column(db.Enum(CameraStatus), nullable=False)
-#     mode = db.Column(db.Enum(CameraMode), nullable=False)
--- server/db_classes/event.py
+++
@@ -1,9 +0,0 @@
-# from flask_sqlalchemy import SQLAlchemy
-
-
-# class Event(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-#     camera_id = db.Column(db.Integer, db.ForeignKey('camera.id'), nullable=False)
-#     type = db.Column(db.Enum(EventType), nullable=False)
-#     timestamp = db.Column(db.DateTime, nullable=False)
