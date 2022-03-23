014e4f9de7a1870ec3be3f6fa72992f84b2ceaf0
jpaetsch <j.paetsch@live.com>
2022-03-23 17:48:28 -0600

    Fixed the tests so that data is passed properly to the api endpoint

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -13,15 +13,14 @@ def test_test_get(client):
     assert data['data'] == 'get_request'
 
 # TODO figure out how to set up this api and test as we were getting json and response data errors
-# def test_event(test_client):
-#     """Test the endpoint for posting events"""
-#     json_data = { 
-#         "user_id": 4,
-#         "camera_id": 2,
-#         "event_type": "IMAGE_CAPTURE_FAILED",
-#         "timestamp": datetime.date(2022, 4, 4)
-#     }
-#     response = test_client.post("/v1/events", json=jsonify(json_data))
-
-#     data = json.loads(response.get_data(as_text=True))
-#     assert data['success'] == True
+def test_event(test_client):
+    """Test the endpoint for posting events"""
+    json_data = { 
+        "user_id": 4,
+        "camera_id": 2,
+        "event_type": "IMAGE_CAPTURE_FAILED",
+        "timestamp": datetime.date(2022, 4, 4)
+    }
+    response = test_client.post("/v1/events", data=json_data)
+    data_dict = json.loads(response.get_data(as_text=True))
+    assert data_dict['success'] == True
