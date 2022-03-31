2645e9db6f64ac509a5819902734e7b8abc4d323
Rudy Patel <rutvik@ualberta.ca>
2022-03-31 13:31:05 -0600

    removed unneccessary test

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -2,19 +2,6 @@ from flask import json
 from server.models.models import Users, Camera
 import datetime
 
-def test_event(test_client):
-    input_data = { 
-        "user_id": 5,
-        "camera_id": 2,
-        "event_type": "IMAGE_CAPTURE_FAILED",
-        "timestamp": datetime.date(2022, 1, 1)
-    }
-    
-    response = test_client.post("/v1/events", data=input_data)
-    response_data = json.loads(response.get_data(as_text=True))
-    
-    assert response_data['success'] == True
-
 def test_heartbeat(test_client):
     cameraID = 2
     response = test_client.get("/v1/heartbeat/" + str(cameraID))
