28837d3fbc3a14b3c8df5a1f57a17174a857218f
Rudy Patel <rutvik@ualberta.ca>
2022-04-01 15:17:52 -0600

    updating Procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn --bind 0.0.0.0:$PORT app:create_app('dev')
+web: gunicorn --bind 0.0.0.0:$PORT app:app
--- app.py
+++ app.py
@@ -1,6 +1,2 @@
 from server.main import create_app
-import os
-
-if __name__ == "__main__":
-    app = create_app(os.getenv('FLASK_CONFIG') or 'dev')
-    app.run()
+app = create_app()
